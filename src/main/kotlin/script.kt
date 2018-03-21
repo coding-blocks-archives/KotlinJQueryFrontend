31a8885a0a1a56c41bffb831e62a709b32a03120
Arnav Gupta <arnav@codingblocks.com>
2018-03-22 05:24:33 +0530

    basic todolist done
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

--- src/main/kotlin/script.kt
+++ src/main/kotlin/script.kt
@@ -2,10 +2,41 @@ package main.kotlin
 
 import kotlin.browser.*
 import kotlin.dom.*
-import jquery.*
+import kotlinx.html.*
+import kotlinx.html.dom.*
+import types.JQuery
+import types.JQueryStatic
+import types.`$`
+import types.jQuery
 
+
+data class Todo(
+		var task: String?,
+		var done: Boolean? = false
+)
 fun main(args: Array<String>) {
     window.onload = {
+        val J = jQuery
         console.log("Started")
+
+        var todos = ArrayList<Todo>()
+
+        val list = J("#todolist")
+        val btn = J("#addtodo")
+        val todo = J("#newtodo")
+
+        fun refresh () {
+            list.empty()
+            for (t in todos) {
+                list.get()[0].append { li { +t.task!! } }
+            }
+        }
+
+        btn.click {
+            todos.add(Todo(task = todo.`val`().toString()))
+            refresh()
+        }
+
+        0
     }
 }
